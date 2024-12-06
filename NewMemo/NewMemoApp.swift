fe22c688f7f8cc3acc7f908c85643d743a563c28
xiaojing <wang.xiaojing.1959@gmail.com>
2024-12-06 20:41:47 +0900

    Initial Commit

---
+++ NewMemo/NewMemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  NewMemoApp.swift
+//  NewMemo
+//
+//  Created by Xiaojing Wang on 2024/12/06.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NewMemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
